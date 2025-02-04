f6a3f14f011c16933b1b3c93233934d0e6d93ea7
Azrael717171 <richardpandog09@gmail.com>
2025-02-04 09:28:05 +0800

    git add --all

--- routes/report.js
+++ routes/report.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const Report = require('../models/report');
 
+
+
 // Create Report
 router.post('/generate', async (req, res) => {
   try {
@@ -23,5 +25,21 @@ router.get('/', async (req, res) => {
     res.status(500).json({ message: 'Failed to fetch reports', error });
   }
 });
+
+// Delete a report by ID
+router.delete('/:id', async (req, res) => {
+  try {
+    const reportId = req.params.id;
+    const result = await Report.findByIdAndDelete(reportId);
+    if (result) {
+      res.status(200).json({ message: 'Report deleted successfully' });
+    } else {
+      res.status(404).json({ message: 'Report not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'An error occurred while deleting the report', error: error.message });
+  }
+});
+
 
 module.exports = router;
